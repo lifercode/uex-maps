3c15d2da7c4986496292c33e0311852eb07e9d61
Andre <lifercode@gmail.com>
2025-03-16 18:56:23 -0300

    feat: ordened contact list

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import { searchByCPF, searchByName } from "../../lib/utils/search";
 import { useContacts } from "../../hooks/use-contacts";
 import { Input } from "../ui/input";
+import { Icon } from "../ui/icon";
 
 export function AppSidebar() {
   const { contacts, selectContact, selectedContact } = useContacts();
   const [searchTerm, setSearchTerm] = useState("");
+  const [ordened, setOrdened] = useState(true);
 
-  const filteredData = contacts.filter((item) => {
-    return (
-      searchByName(item.name, searchTerm) || searchByCPF(item.cpf, searchTerm)
-    );
-  });
+  const filteredData = useMemo(() => {
+    const filtered = contacts.filter((item) => {
+      return (
+        searchByName(item.name, searchTerm) || searchByCPF(item.cpf, searchTerm)
+      );
+    });
+
+    filtered.sort((a, b) => {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return ordened ? -1 : 1;
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return ordened ? 1 : -1;
+      return 0;
+    });
+
+    return filtered;
+  }, [contacts, searchTerm, ordened]);
 
   return (
     <>
@@ -27,7 +39,13 @@ export function AppSidebar() {
           />
         </div>
       </div>
-      <div className="overflow-y-auto h-[calc(100vh-96px-4rem)]">
+      <button
+        onClick={() => setOrdened(!ordened)}
+        className="hover:bg-secondary-100 dark:hover:bg-secondary-700 hover:bg-opacity-30 dark:hover:bg-opacity-30 w-full flex justify-center items-center h-7"
+      >
+        <Icon name={ordened ? 'keyboard_arrow_down' : 'keyboard_arrow_up'} />
+      </button>
+      <div className="overflow-y-auto h-[calc(100vh-96px-5.75rem)]">
         <div className="flex flex-col">
           {filteredData.length < 1 && (
             <div className="flex items-center justify-center opacity-60 py-5">
